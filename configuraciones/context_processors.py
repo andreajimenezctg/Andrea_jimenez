bb6423405b005e4fa4430807b762b6df3412630b
unknown <andreajimenezctg>
2026-03-17 07:04:34 -0500

    Unificación de Navbar en todos los módulos y optimización responsive vertical

--- configuraciones/context_processors.py
+++ configuraciones/context_processors.py
@@ -4,8 +4,13 @@ def global_config(request):
     """
     Context processor to make global variables available in all templates.
     """
+    es_cliente = False
+    if request.user.is_authenticated:
+        es_cliente = request.user.groups.filter(name='Cliente').exists()
+        
     return {
         'whatsapp_number': getattr(settings, 'WHATSAPP_NUMBER', '573014717412'),
         'paypal_client_id': getattr(settings, 'PAYPAL_CLIENT_ID', 'sb'),
         'paypal_currency': getattr(settings, 'PAYPAL_CURRENCY', 'USD'),
+        'es_cliente': es_cliente,
     }
